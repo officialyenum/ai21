f82ca43f46eebc1984bfbeb3c1fd10249b84661c
officialyenum <oponechukwuyenum@gmail.com>
2023-02-21 19:00:38 +0100

    Modified Doc

---
+++ lib/esm/types/constants/index.d.ts
@@ -0,0 +1,11 @@
+export declare const useHeaders: (token: string) => {
+    Authorization: string;
+    "Content-Type": string;
+};
+export declare const useBody: (text: string) => {
+    text: string;
+};
+export declare const useSummarize: () => {
+    url: string;
+};
+//# sourceMappingURL=index.d.ts.map
