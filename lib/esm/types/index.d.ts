c829d3057b0aa2d05912085e06aecb1ed1e12e85
officialyenum <oponechukwuyenum@gmail.com>
2023-02-23 09:01:21 +0100

    Clean Build

--- lib/esm/types/index.d.ts
+++ lib/esm/types/index.d.ts
@@ -1,38 +1,70 @@
-interface TextObject {
-    text: string | undefined;
-    type?: string;
-    restaurant: string | undefined;
-    review: string | undefined;
-}
+import { ISummaryBody, ISummarizeResponse } from './library';
 export declare class AI21 {
     token?: string;
     constructor(token?: string);
-    validateSummarize(data?: TextObject): {
-        data: TextObject;
+    validateSummarize(data?: ISummaryBody): {
+        data: ISummaryBody;
         token: string;
     };
     /**
      *
-     * @param data
+     * @param ISummaryBody
      *
-     * @returns
+     * @returns ISummarizeResponse
      */
-    summarize(data?: TextObject): Promise<{
-        status: string;
-        message: string;
-        data: any;
-    } | undefined>;
+    summarize(data?: ISummaryBody): Promise<ISummarizeResponse | undefined>;
     getResponse(status: string, message: string, data: any): {
         status: string;
         message: string;
         data: any;
     };
 }
-export declare function helloWorld(): string;
-export declare function goodBye(): string;
 declare const _default: {
-    helloWorld: typeof helloWorld;
-    goodBye: typeof goodBye;
+    useBody: (text: string) => {
+        text: string;
+    };
+    useHeaders: (token: string) => {
+        Authorization: string;
+        "Content-Type": string;
+    };
+    useSummarize: (type: string) => {
+        url: string;
+    };
+    useSummarizeBody: (data: any) => {
+        prompt: string;
+        numResults: number;
+        maxTokens: number;
+        temperature: number;
+        topKReturn: number;
+        topP: number;
+        countPenalty: {
+            scale: number;
+            applyToNumbers: boolean;
+            applyToPunctuations: boolean;
+            applyToStopwords: boolean;
+            applyToWhitespaces: boolean;
+            applyToEmojis: boolean;
+        };
+        frequencyPenalty: {
+            scale: number;
+            applyToNumbers: boolean;
+            applyToPunctuations: boolean;
+            applyToStopwords: boolean;
+            applyToWhitespaces: boolean;
+            applyToEmojis: boolean;
+        };
+        presencePenalty: {
+            scale: number;
+            applyToNumbers: boolean;
+            applyToPunctuations: boolean;
+            applyToStopwords: boolean;
+            applyToWhitespaces: boolean;
+            applyToEmojis: boolean;
+        };
+        stopSequences: string[];
+    } | {
+        text: any;
+    };
     AI21: typeof AI21;
 };
 export default _default;
---
+++ lib/esm/types/library/ISummary.d.ts
@@ -0,0 +1,12 @@
+export interface ISummaryBody {
+    text?: string | undefined;
+    type?: string;
+    restaurant?: string | undefined;
+    review?: string | undefined;
+}
+export interface ISummarizeResponse {
+    status: string;
+    message: string;
+    data?: any | undefined;
+}
+//# sourceMappingURL=ISummary.d.ts.map
